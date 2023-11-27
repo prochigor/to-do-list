6c0a3a633813e94d57878100b22387d8706ac7aa
Ihor Prochnickiy <2prochigor@gmail.com>
2023-11-27 10:54:52 +0200

    base setting project

--- to_do/apps.py
+++ to_do/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ToDoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'to_do'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "to_do"
