29509c7aeacd20e3ba2ffc5cc38e4731a3550212
Ihor Prochnickiy <2prochigor@gmail.com>
2023-11-27 11:09:45 +0200

    add models

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Todo(models.Model):
+    content = models.CharField(max_length=100)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True)
+    is_done = models.BooleanField()
+    tag = models.ManyToManyField(Tag, related_name="todos")
