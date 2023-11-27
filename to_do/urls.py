b1e875519ff260ac4a9171b047e91f4bea0182ed
Ihor Prochnickiy <2prochigor@gmail.com>
2023-11-27 13:23:54 +0200

    add CRUD fot tags

--- to_do/urls.py
+++ to_do/urls.py
@@ -4,21 +4,37 @@ from to_do.views import (
     TodoListView,
     TOdoCreateView,
     TOdoUpdateView,
-    TOdoDeleteView
+    TOdoDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
 )
 
 urlpatterns = [
     path("", TodoListView.as_view(), name="index"),
-    path("todos/create", TOdoCreateView.as_view(), name="to-do-create"),
+    path("todos/create/", TOdoCreateView.as_view(), name="to-do-create"),
     path(
-        "todos/<int:pk>/update",
+        "todos/<int:pk>/update/",
         TOdoUpdateView.as_view(),
         name="to-do-update"
     ),
     path(
-        "todos/<int:pk>/delete",
+        "todos/<int:pk>/delete/",
         TOdoDeleteView.as_view(),
         name="to-do-delete"
+    ),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update"
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete"
     ),
 ]
 
--- to_do/views.py
+++ to_do/views.py
@@ -6,9 +6,10 @@ from to_do.models import Tag, Todo
 
 class TodoListView(generic.ListView):
     model = Todo
+    queryset = Todo.objects.all()
     template_name = "to_do/index.html"
     ordering = ["-created"]
-    paginate_by = 10
+    paginate_by = 5
 
 
 class TOdoCreateView(generic.CreateView):
@@ -26,3 +27,27 @@ class TOdoUpdateView(generic.UpdateView):
 class TOdoDeleteView(generic.DeleteView):
     model = Todo
     success_url = reverse_lazy("to_do:index")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 10
+    queryset = Tag.objects.all()
+    fields = "__all__"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("to_do:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("to_do:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("to_do:tag-list")
